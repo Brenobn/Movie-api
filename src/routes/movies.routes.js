92494e2e6be8c8243a55314f85d0e8d42c4f0fa1
Brenobn <brenobragabn@gmail.com>
2023-12-01 10:42:32 -0300

    Adding new route using delete method

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -8,6 +8,8 @@ const moviesController = new MoviesController();
 
 moviesRoutes.post("/:user_id", moviesController.create);
 moviesRoutes.get("/:id", moviesController.show);
+moviesRoutes.delete("/:id", moviesController.delete);
+
 
 
 module.exports = moviesRoutes;
