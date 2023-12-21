74e58c8da28cd2ee7fbc519b24c26545f355ef43
Brenobn <brenobragabn@gmail.com>
2023-12-21 13:01:52 -0300

    Importing authentication middleware into tags.routes.js

--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 
 const TagsController = require("../controllers/TagsController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const tagsRoutes = Router();
 
