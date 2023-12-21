ba112aec9d580b5b7b4d44baa5f65938cdd83687
Brenobn <brenobragabn@gmail.com>
2023-12-21 13:02:57 -0300

    Passing authentication middleware for all routes from movie.routes.js

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -6,8 +6,10 @@ const moviesRoutes = Router();
 
 const moviesController = new MoviesController();
 
+moviesRoutes.use(ensureAuthenticated);
+
 moviesRoutes.get("/", moviesController.index);
-moviesRoutes.post("/:user_id", moviesController.create);
+moviesRoutes.post("/", moviesController.create);
 moviesRoutes.get("/:id", moviesController.show);
 moviesRoutes.delete("/:id", moviesController.delete);
 
