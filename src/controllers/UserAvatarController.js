61349f9f3204d21d1ade749e7ef235895bc96d1f
Brenobn <brenobragabn@gmail.com>
2023-12-22 11:39:56 -0300

    Importing knex and creating async update function using knex

--- src/controllers/UserAvatarController.js
+++ src/controllers/UserAvatarController.js
@@ -1,4 +1,13 @@
+const knex = require("../database/knex");
+
 class UserAvatarController {
+  async update(request, response) {
+    const user_id = request.user.id;
+    const avatarFilename = request.file.filename;
+
+    const user = await knex("users")
+      .where({ id: user_id }).first();
+  }
 
 }
 
